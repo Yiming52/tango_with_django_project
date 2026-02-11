88303d2ce3fb83c5ab2b900bef8c57302dc963c0
Yiming52 <2064826253@qq.com>
2026-02-11 08:34:42 +0000

    Chapter 4.2: configure static files and add image

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'rango/index.html')
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
-
+def about(request):
+    return render(request, 'rango/about.html')
